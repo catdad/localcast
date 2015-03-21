b3705e938573729c9e71015e6d4353434f969800
Kiril Vatev <vatev.1@gmail.com>
2015-03-21 13:21:30 -0400

    adding gulp-util

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,14 +2,16 @@
 
 var gulp = require('gulp'),
     less = require('gulp-less'),
-    sourcemaps = require('gulp-sourcemaps');
+    sourcemaps = require('gulp-sourcemaps'),
+    gutil = require('gulp-util');
 
 gulp.task('less', function() {
     return gulp.src('./less/style.less')
         .pipe(sourcemaps.init())
         .pipe(less())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./public'));
+        .pipe(gulp.dest('./public'))
+        .on('error', gutil.log);
 });
 
 gulp.task('watch', ['less'], function() {
