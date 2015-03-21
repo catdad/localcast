fc95c15c51b9764ac7871de5274c0468f47fd6db
Kiril Vatev <vatev.1@gmail.com>
2015-03-21 12:50:18 -0400

    adding gulp-sourcemaps

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,14 @@
 /* jshint node: true */
 
 var gulp = require('gulp'),
-    less = require('gulp-less');
+    less = require('gulp-less'),
+    sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('less', function() {
     return gulp.src('./less/style.less')
+        .pipe(sourcemaps.init())
         .pipe(less())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('./public'));
 });
 
