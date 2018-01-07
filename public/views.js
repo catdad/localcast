6ea8982270d2f758fe4c30977590c2eadd37c868
Kiril Vatev <vatev.1@gmail.com>
2018-01-07 15:00:53 -0500

    extracting video splash to a new component

--- public/views.js
+++ public/views.js
@@ -47,122 +47,13 @@ var views = {
     Modal: function(contentDom, onOpen, origin) {
         window.STATE.emit('modal:open', contentDom, onOpen, origin);
     },
-    videoModal: function(resource, name){
+    videoModal: function(resource, name) {
         window.STATE.emit('video:play', resource, name);
     },
-    controlsModal: function(ev, thumb, resource, name, domTrigger){
-        console.log('clicked', name);
-        
-        var modal = views.elem('div'),
-            container = views.elem('div'),
-            image = views.elem('img', { className: 'thumb' }),
-            title = views.elem('div', { className: 'title', text: name }),
-            modalWrapper;
-        
-        container.appendChild(title);
-        
-        // get the origin to use for animation
-        var triggerBB = domTrigger.getBoundingClientRect(),
-            origin = {
-                x: (triggerBB.left + triggerBB.right) / 2,
-                y: (triggerBB.top + triggerBB.bottom) / 2
-            };
-        
-        var playLocalButton = document.createElement('button');
-        var playCastButton = document.createElement('button');
-      
-        // first, queue the image to load, and keep track here
-        var modalIsOpen = false,
-            imageIsLoaded = false;
-        
-        // this function should be executed after image has loaded and tranition has ended
-        var imageOnLoad = function(wrapper) {
-            // make sure the modal is empty
-            while(wrapper && wrapper.firstChild) { 
-                wrapper.removeChild(wrapper.firstChild);
-            }
-            
-            wrapper.appendChild(modal);
-            
-            // get necessary dimensions
-            var cWidth = modal.clientWidth,
-                width = image.width,
-                height = image.height;
-            
-            // insert the image first
-            modal.appendChild(image);
-            
-            // insert the two buttons created earlier
-            container.appendChild(playLocalButton);
-            container.appendChild(playCastButton);
-            
-            modal.appendChild(container);
-            
-            var containerHeight = container.offsetHeight;
-            
-            // let's animate the height transition as well
-            window.UTIL.raf(function() {
-                modal.style.height = ( cWidth * height / width ) + containerHeight + 'px';
-            });
-        };
-        
-        // add an onload callback and a source to the image
-        image.onload = function() {
-            if (modalIsOpen) {
-                imageOnLoad(modalWrapper);
-            } else {
-                imageIsLoaded = true;
-            }
-        };
-        image.src = thumb;
-        
-        // Build the play and cast buttons while we wait for the image
-        playLocalButton.className = 'play';
-        playCastButton.className = 'cast';
-        
-        var playIcon = views.elem('span', { className: 'icon icon-play' }),
-            playText = document.createTextNode('Play');
-        var castIcon = views.elem('span', { className: 'icon icon-cast' }),
-            castText = document.createTextNode('Cast');
-        
-        playLocalButton.appendChild(playIcon);
-        playLocalButton.appendChild(playText);
-        playCastButton.appendChild(castIcon);
-        playCastButton.appendChild(castText);
-        
-        playLocalButton.onclick = function(){
-            views.videoModal(resource, name);
-        };
-        playCastButton.onclick = function(){
-            function browserCast() {
-                chromecast.startCast(resource, {
-                    title: name,
-                    images: [{ url: thumb }]
-                });
-            }
-            
-            function serverCast() {
-                window.STATE.emit('modal:close');
-                server.playNew(resource, name, thumb);
-            }
-            
-            (chromecast.isAvailable() && false) ? browserCast() : serverCast();
-        };
-        
-        views.Modal(modal, function onOpen(wrapper){
-            modalWrapper = wrapper;
-            
-            if (imageIsLoaded) {
-                imageOnLoad(wrapper);
-            } else {
-                modalIsOpen = true;
-                // add spinner while the image loads
-                wrapper.appendChild(views.elem('div', { className: 'loading' }));
-            }
-            
-        }, origin);
+    controlsModal: function(ev, thumb, resource, name, domTrigger) {
+        window.STATE.emit('splash', ev, thumb, resource, name, domTrigger);
     },
-    deviceSelectModal: function(list, onSelect, onCancel){
+    deviceSelectModal: function(list, onSelect, onCancel) {
         var div = views.elem('div', { className: 'list'}),
             heading = views.elem('div', { className: 'heading', text: 'Select a device:' }),
             selected = false,
