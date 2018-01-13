980b4ce782e41171d4df141c004fc1378af9d4c9
Kiril Vatev <vatev.1@gmail.com>
2018-01-13 16:52:09 -0500

    player is now multi-element , showing video name on top of video when hovered

--- public/components/player.js
+++ public/components/player.js
@@ -20,9 +20,38 @@
     }
     
     STATE.on('video:play', function (resource, name) {
-        var vid = UTIL.elem('video', { className: 'modal video' });
+        var player = UTIL.elem('div', { className: 'player' });
+        var container = UTIL.elem('div', { className: 'player-container' });
+        var title = UTIL.elem('div', { className: 'video-title', text: name });
+        var vid = UTIL.elem('video');
+
         vid.src = resource;
         vid.controls = 'controls';
+        
+        container.appendChild(vid);
+        container.appendChild(title);
+        
+        player.appendChild(container);
+        
+        var showTitle = (function () {
+            var timeout;
+            var time = 1000 * 3;
+            var className = 'hidden';
+            
+            function hide() {
+                title.classList.add(className);
+            }
+            
+            return function show(timeOverride) {
+                title.classList.remove(className);
+                
+                if (timeout) {
+                    clearTimeout(timeout);
+                }
+                
+                timeout = setTimeout(hide, timeOverride || time);
+            };
+        }());
         
         function togglePlaying() {
             if (vid.ended) {
@@ -42,6 +71,14 @@
             togglePlaying();
         }
         
+        function onVideoMove(ev) {
+            showTitle();
+        }
+        
+        function onVideoOut(ev) {
+            showTitle(100);
+        }
+        
         function onKeyPress(ev) {
             // space bar pauses and plays the video
             if (ev.which === 32 || ev.keyCode === 32) {
@@ -51,6 +88,8 @@
         
         function tearDown() {
             vid.removeEventListener('click', onVideoClick);
+            vid.removeEventListener('mousemove', onVideoMove);
+            vid.removeEventListener('mouseout', onVideoOut);
             window.removeEventListener('keypress', onKeyPress);
             
             vid.src = null;
@@ -63,6 +102,8 @@
 
             // add convenient play/pause controls
             vid.addEventListener('click', onVideoClick);
+            vid.addEventListener('mousemove', onVideoMove);
+            vid.addEventListener('mouseout', onVideoOut);
             window.addEventListener('keypress', onKeyPress);
 
             STATE.once('modal:closing', function () {
@@ -74,6 +115,7 @@
             
             UTIL.once(vid, 'playing', function () {
                 wrapper.classList.add('dim');
+                showTitle();
             });
             
             vid.addEventListener('ended', function() {
@@ -89,6 +131,6 @@
         }
         
         STATE.once('modal:closed', tearDown);
-        STATE.emit('modal:open', vid, onModalOpen);        
+        STATE.emit('modal:open', player, onModalOpen);        
     });
 }(window));
