012efd7b6364d5e1f90dee4b666a67788e6a2862
Kiril Vatev <vatev.1@gmail.com>
2018-06-02 09:34:37 -0400

    asynchronously getting all the stats

--- server/utils/fsPlus.js
+++ server/utils/fsPlus.js
@@ -3,26 +3,48 @@
 var fs = require("fs");
 var path = require("path");
 
+function statSeries(dir, data, done) {
+    var stats = [];
+
+    function statPromise(filePath) {
+        return new Promise(function (resolve, reject) {
+            fs.stat(filePath, function (err, stat) {
+                if (err) {
+                    return reject(err);
+                }
+
+                return resolve(stat);
+            });
+        });
+    }
+
+    data.reduce(function (prom, el) {
+        var thisPath = path.resolve(dir, el);
+
+        return prom.then(function () {
+            return statPromise(thisPath).then(function (stat) {
+                stats.push({
+                    name: el,
+                    path: thisPath,
+                    isFile: stat.isFile(),
+                    isDirectory: stat.isDirectory()
+                });
+            });
+        });
+    }, Promise.resolve()).then(function () {
+        done(null, stats);
+    }).catch(function (err) {
+        done(err);
+    });
+}
+
 var dirStats = function getDir(dir, callback){
     fs.readdir(dir, function(err, data){
         if (err) {
-            callback(err);
-            return;
+            return callback(err);
         }
 
-        var stats = [];
-        data.forEach(function(el, i, arr){
-            var thisPath = path.resolve(dir, el);
-            var thisStat = fs.statSync( thisPath );
-            stats.push({
-                name: el,
-                path: thisPath,
-                isFile: thisStat.isFile(),
-                isDirectory: thisStat.isDirectory()
-            });
-        });
-
-        callback(null, stats);
+        statSeries(dir, data, callback);
     });
 };
 
